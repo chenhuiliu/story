063079b1037d0e98cec180d4fbba8cbfd997ef16
fenglu.li <fenglu.li@tendcloud.com>
2017-07-10 14:10:48 +0800

    add

--- client/src/component/editorLike/main.js
+++ client/src/component/editorLike/main.js
@@ -1,7 +1,22 @@
 import './css.css';
+import Mustache from 'mustache';
 module.exports = {
     html: require("./html.html"),
     load: function () {
+        // debugger
         TDM.ui.loading.hide();
+        // debugger
+        debugger
+        this.showTiitle();
+    },
+    showTiitle() {
+        var template = document.querySelector('#clientResourceModules-template').innerHTML;
+        Mustache.parse(template); // optional, speeds up future uses 
+        var rendered = Mustache.render(template, {
+            name: "Luke"
+        });
+        
+        document.getElementById('clientResourceModules-target').innerHTML = rendered;
+        // $('#clientResourceModules-target').html(rendered);
     }
 }
